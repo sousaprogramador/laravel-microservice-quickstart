aac3d747083f2c6829503283d462e017257bb78f
Mateus Sousa <sousa.programador@gmail.com>
2020-11-24 17:36:31 -0300

    inital test unit Category

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -13,5 +13,5 @@ class Genre extends Model
     protected $fillable = ['name', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = ['id' => 'string'];
-    protected $incrementing = false;
+    public $incrementing = false;
 }
